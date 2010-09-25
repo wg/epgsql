98ba9dd8a2a6e370c40e5707e9e0b59f71da582f
Anton Lebedevich <mabrek@gmail.com>
2010-09-25 16:18:38 +0400

    int4 array parsing implemented

--- src/pgsql_text.erl
+++ src/pgsql_text.erl
@@ -5,7 +5,22 @@
 -export([decode/2]).
 
 decode('_int4', Raw) ->
-    Raw;
+    int4_array(Raw);
 
 decode(_Other, Value) ->
     Value.
+
+int4_array(<<${, Rest/binary>>) ->
+    int4_array(Rest, [], []).
+
+int4_array(<<$,, Rest/binary>>, Field, Accumulator) ->
+    int4_array(Rest, [], [field_to_integer(Field) | Accumulator]);
+
+int4_array(<<$}>>, Field, Accumulator) ->
+    lists:reverse([field_to_integer(Field) | Accumulator]);
+    
+int4_array(<<Char, Rest/binary>>, Field, Accumulator) ->
+    int4_array(Rest, [Char | Field], Accumulator).
+
+field_to_integer(Field) ->
+    list_to_integer(lists:reverse(Field)).
