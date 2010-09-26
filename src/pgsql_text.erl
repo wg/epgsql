b7aa9be4832e0bfb40430fa055f54192747b32f1
Anton Lebedevich <mabrek@gmail.com>
2010-09-26 13:21:29 +0400

    int4 array encoding added

--- src/pgsql_text.erl
+++ src/pgsql_text.erl
@@ -6,7 +6,11 @@
 
 -include("pgsql_protocol.hrl").
 
+encode('_int4', List) when is_list(List) ->
+    encode_int_array(lists:reverse(List), []);
+
 encode(_Type, Value) -> encode(Value).
+
 
 encode(A) when is_atom(A)    -> encode(atom_to_list(A));
 encode(B) when is_binary(B)  -> <<(byte_size(B)):?int32, B/binary>>;
@@ -16,23 +20,40 @@ encode(L) when is_list(L)    ->
     Bin = list_to_binary(L),
     <<(byte_size(Bin)):?int32, Bin/binary>>.
 
+
+encode_int_array([Int | Rest], []) ->
+    encode_int_array(Rest, [int_to_element(Int), $]]);
+
+encode_int_array([Int | Rest], Accumulator) when is_integer(Int) ->
+    encode_int_array(Rest, [[int_to_element(Int), $,] | Accumulator]);
+
+encode_int_array([], Accumulator) ->
+    encode(["array[" | Accumulator]).
+
+
+int_to_element(Int) when is_integer(Int) ->
+    list_to_binary(integer_to_list(Int)).
+
+
 decode('_int4', Raw) ->
-    int4_array(Raw);
+    decode_int_array(Raw);
 
 decode(_Other, Value) ->
     Value.
 
-int4_array(<<${, Rest/binary>>) ->
-    int4_array(Rest, [], []).
 
-int4_array(<<$,, Rest/binary>>, Element, Accumulator) ->
-    int4_array(Rest, [], [element_to_int4(Element) | Accumulator]);
+decode_int_array(<<${, Rest/binary>>) ->
+    decode_int_array(Rest, [], []).
 
-int4_array(<<$}>>, Element, Accumulator) ->
-    lists:reverse([element_to_int4(Element) | Accumulator]);
+decode_int_array(<<$,, Rest/binary>>, Element, Accumulator) ->
+    decode_int_array(Rest, [], [element_to_int(Element) | Accumulator]);
+
+decode_int_array(<<$}>>, Element, Accumulator) ->
+    lists:reverse([element_to_int(Element) | Accumulator]);
     
-int4_array(<<Char, Rest/binary>>, Element, Accumulator) ->
-    int4_array(Rest, [Char | Element], Accumulator).
+decode_int_array(<<Char, Rest/binary>>, Element, Accumulator) ->
+    decode_int_array(Rest, [Char | Element], Accumulator).
 
-element_to_int4(Element) ->
+
+element_to_int(Element) ->
     list_to_integer(lists:reverse(Element)).
