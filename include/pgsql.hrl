aec760eb5ab8d27d0e75bd2400f9aeb157ab96a6
Steven Gravell <steve@mokele.co.uk>
2011-02-18 08:05:18 +0000

    add #error.type atom

--- include/pgsql.hrl
+++ include/pgsql.hrl
@@ -1,4 +1,4 @@
 -record(column,    {name, type, size, modifier, format}).
 -record(statement, {name, columns, types}).
 
--record(error,  {severity, code, message, extra}).
+-record(error,  {severity, type, code, message, extra}).
